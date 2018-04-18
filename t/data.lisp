afeecabe08dafd74e7e4e4a03a90c6b22e2985a6
tamura shingo <tamura.shingo@gmail.com>
2018-04-18 13:06:31 +0900

    add test

--- t/data.lisp
+++ t/data.lisp
@@ -7,6 +7,7 @@
                 :set-environment))
 (in-package :reddit-test.data)
 
+(clsql:locally-enable-sql-reader-syntax)
 
 (plan nil)
 
@@ -67,5 +68,81 @@
   (is (valid-user-p 999999 :return-sn T)
       nil))
 
+;; add-user
+(subtest "add-user"
+  (diag "add normal user")
+  ;; fake: nil
+  ;; valid-user-p: nil
+  (add-user "add-user-1" "add-user-1@sample.com" "password" "192.168.0.1")
+  (let ((user (get-user "add-user-1")))
+    (is (reddit.view-defs:user-name user)
+        "add-user-1")
+    (is (reddit.view-defs:user-emai user)
+        "add-user-1@sample.com")
+    (is (slot-value user 'reddit.view-defs::password)
+        "password")
+    (is (reddit.view-defs:user-karma user)
+        0)
+    (is (reddit.view-defs:user-ip user)
+        "192.168.0.1"))
+
+
+  (diag "add fake user")
+  ;; fake: t
+  ;; valid-user-p: nil
+  (add-user "add-user-2" "add-user-2@sample.com" "password2" "192.168.0.2" T)
+  (let ((user (get-user "add-user-2")))
+    (is (reddit.view-defs:user-name user)
+        "add-user-2")
+    (is (reddit.view-defs:user-emai user)
+        "add-user-2@sample.com")
+    (is (slot-value user 'reddit.view-defs::password)
+        "password2")
+    (is (reddit.view-defs:user-karma user)
+        0)
+    (is (reddit.view-defs:user-ip user)
+        "192.168.0.2")
+    (ok (< (reddit.view-defs:user-id user) 0)))
+
+  (diag "override fake user to normal user")
+  ;; fake: nil
+  ;; valid-user-p: t
+  ;; fake-user-p: t
+  (add-user "add-user-2" "add-user-2-fake@sample.com" "password-fake" "192.168.0.3")
+  (let ((user (get-user "add-user-2"))
+        (old-user (get-user "add-user-22")))
+    (is (reddit.view-defs:user-name user)
+        "add-user-2")
+    (is (reddit.view-defs:user-emai user)
+        "add-user-2-fake@sample.com")
+    (is (slot-value user 'reddit.view-defs::password)
+        "password-fake")
+    (is (reddit.view-defs:user-karma user)
+        0)
+    (is (reddit.view-defs:user-ip user)
+        "192.168.0.3")
+    (ok (> (reddit.view-defs:user-id user) 0))
+
+    (is (reddit.view-defs:user-name old-user)
+        "add-user-22")
+    (is (reddit.view-defs:user-emai old-user)
+        "add-user-2@sample.com")
+    (is (slot-value old-user 'reddit.view-defs::password)
+        "password2")
+    (is (reddit.view-defs:user-karma old-user)
+        0)
+    (is (reddit.view-defs:user-ip old-user)
+        "192.168.0.2")
+    (ok (< (reddit.view-defs:user-id old-user) 0)))
+
+  (diag "user already registered")
+  ;; fake: nil
+  ;; valid-user-p: t
+  ;; vake-user-p: nil
+  (handler-case (add-user "add-user-1" "add-user-1-error@sample.com" "error" "192.168.0.4")
+    (error () (ok "user already exists"))
+    (:no-error () (fail "no signal"))))
 
 (finalize)
+
+(clsql:locally-disable-sql-reader-syntax)
