d5cf53b67c7d187c56abb4e0eb39d684fed3dd1c
tamura shingo <tamura.shingo@gmail.com>
2023-08-17 13:20:36 +0900

    add db test

--- test/data-test.lisp
+++ test/data-test.lisp
@@ -25,10 +25,31 @@
   (reddit.main::disconnect-database)
   (format T "ok"))
 
-(deftest reigster-user-and-check
-  (testing "register user"
+(deftest register-user-and-check
+  (testing "register normal user"
     ;; prepare
-    (reddit.data::add-user "tamu" "tamu@example.com" "password" "127.0.0.1")
+    (add-user "tamu1" "tamu1@example.com" "password1" "127.0.0.1")
 
     ;; validate
-    (ok (reddit.data::valid-login-p "tamu" "password"))))
+    (ok (valid-login-p "tamu1" "password1"))
+    (ok (valid-user-p "tamu1"))
+    (ng (fake-user-p "tamu1")))
+
+  (testing "register fake user"
+    ;; prepare
+    (add-user "tamu2" "tamu2@example.com" "password2" "127.0.0.1" T)
+
+    ;; validate
+    (ok (valid-login-p "tamu2" "password2"))
+    (ok (valid-user-p "tamu2"))
+    (ok (fake-user-p "tamu2")))
+
+  (testing "change fake user to normal user"
+    ;; prepare
+    (add-user "tamu3" "tamu3@example.com" "password3" "127.0.0.1" T)
+    (add-user "tamu3" "tamu3@example.com" "password3" "127.0.0.1")
+
+    ;; validate
+    (ok (valid-login-p "tamu3" "password3"))
+    (ok (valid-user-p "tamu3"))
+    (ng (fake-user-p "tamu3"))))
