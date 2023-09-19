9673f17f4c5dc6219da613b2bf7edfaedb0ee8eb
tamura shingo <tamura.shingo@gmail.com>
2023-09-19 18:43:52 +0900

    add qlot and github actions cache

--- script/docker/entrypoint.test.sh
+++ script/docker/entrypoint.test.sh
@@ -3,4 +3,4 @@ set -e
 
 export CL_SOURCE_REGISTRY=/reddit/
 
-ros run -e "(ql:quickload :reddit)" -e "(ql:quickload :reddit-db)" -e "(ql:quickload :reddit-test)" -e "(asdf:test-system :reddit-test)"
+qlot exec ros run -e "(ql:quickload :reddit)" -e "(ql:quickload :reddit-db)" -e "(ql:quickload :reddit-test)" -e "(asdf:test-system :reddit-test)"
